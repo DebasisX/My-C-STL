b3df79b62dc7439895c3ddd487619cd2460ad5ad
DebasisX <115938061+DebasisX@users.noreply.github.com>
2024-05-04 02:57:20 +0530

    Add files via upload
    Added DFS support. Enjoy!
    Dijkstra, Cyclic, BellmanFord  (coming soon)

--- src/graph/graph.c
+++ src/graph/graph.c
@@ -1,24 +1,23 @@
 #include "graph.h"
-int main()
-{
+#include <stdio.h>
+
+int main() {
     // A simple implementation for the above.
-    graph* x[3];
-    int arr0[2] = {14, 27};
-    int arr1[1] = {14};
-    int arr2[1] = {56};
+    graph* x[5];
+    int arr0[2] = {27, 14};
+    int arr1[2] = {14, 18};
+    int arr2[4] = {56, 27, 18, 21};
+    int arr3[2] = {21, 56};
+    int arr4[1] = {18};
     x[0] = insert(56, 'A', 2, arr0);
-    x[1] = insert(27, 'B', 1, arr1);
-    x[2] = insert(14, 'C', 1, arr2);
-    
-    for (int j = 0; j < 3; j++)
-    { 
-        // Displaying.
-        printf("X is :%c \n", (char)x[j]->vertice);
-        for (int i = 0; i < x[j]->num; i++)
-        {
-            printf("EDGE from X is :%d \n", x[j]->edges[i]);
-        }
-    }
-    freeG(x, 3);
+    x[1] = insert(27, 'B', 2, arr1);
+    x[2] = insert(14, 'C', 4, arr2);
+    x[3] = insert(18, 'D', 2, arr3);
+    x[4] = insert(21, 'E', 1, arr4);
+    alloc_visited(5);
+    dfs(x, 5, 27);
+    freeG(x, 5);
     return 0;
 }
+
+
--- src/graph/graph.h
+++ src/graph/graph.h
@@ -1,7 +1,8 @@
-#pragma clang diagnostic ignored "-Wint-conversion"   // Silencing warnings from clang. Lol!
-#pragma clang diagnostic ignored "-Wvoid-pointer-to-int-cast"
-#pragma clang diagnostic ignored "-Warray-bounds"
+#pragma clang diagnostic ignored "-Weverything"   // Silencing warnings from clang. Lol!
 
+
+#include <stdbool.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 // To be used by someone who has some expertise with memory allocation and graphs.
@@ -12,6 +13,12 @@ typedef struct graph
     int num; // No. of edges.
     int *edges; // Pointer to the array of edges.
 } graph;
+int arr[];
+int initial = 0;
+void alloc_visited(int n)
+{  
+    arr[n] = malloc(sizeof(int) * n);
+}
 
 graph* insert(int id, void* vertice, int num, int arr[]) // Pass in Vertice can be Character, Integer, String or any Custom Data Type 
 // along with no. of edges and array to which it would be connected.
@@ -36,3 +43,72 @@ void freeG(graph* x[], int edges) // Pass in pointer to graph and no. of edges t
     }
     // All memory will be freed including the pointer to the graph.
 }
+bool cyclic(graph** x, int num)
+{
+    // vertices can be accessed by x[i]->edges[j]  where x[i] is the vertice and x->edges[j] are the vertices. and num is the number of vertices 
+    // and x->num is the no. of edges associated with the vertice x[i].
+    return false;
+}
+void dfs(graph** x, int num, int id) // Don't try to understand, it was pure instinct as I have never looked at generalised algorithms
+// these algorithms are made by me without reference so ignore if there is something off but be sure it gets the job done correctly. 
+{
+    int total = 0;
+    for(int i = 0; i < num; i++)
+    {
+        total += x[i]->num;
+    }
+    int visited[num][total];
+    for (int i = 0; i < num; i++) {
+    for (int j = 0; j < total; j++) {
+        visited[i][j] = 0;
+        }
+    }
+    
+    for(int i = 0; i < num; i++) // index of vertice
+    {
+        for(int j = 0; j < x[i]->num; j++) // edges
+        {
+            if (x[i]->id == id){
+                visited[i][j] = 1;
+            }
+        }
+    }
+    for(int i = 0; i < num; i++) // index of vertice
+    {
+        for(int j = 0; j < x[i]->num; j++) // edges
+        {
+            if (visited[i][j] == 1)
+            {
+                printf("Vertice ID from %d to %d visited = %d. \n", x[i]->id, x[i]->edges[j], visited[i][j]);
+                arr[initial] = x[i]->id; 
+                initial++;
+            }
+        }
+    }
+    for(int i = 0; i < num; i++) // index of vertice
+    {
+        for(int j = 0; j < x[i]->num; j++) // edges
+        {
+            if (visited[i][j] == 1)
+            {
+                id = x[i]->edges[j];
+                int z = 0;
+                for(int temp = 0; temp < initial; temp++)
+                {
+                    if (arr[temp] == id)
+                    {
+                        z++;
+                    }
+                } 
+                if(z == 0)
+                {
+                    arr[initial] = x[i]->edges[j];
+                    initial++;
+                    dfs(x, num, id);
+                }
+                
+            }
+        }  
+    }
+}
+
