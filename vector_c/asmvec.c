60a4f694f0228568cb90048d5f8f44c62a7c3e45
Debasis Sikdar <115938061+DebasisX@users.noreply.github.com>
2025-06-04 04:30:21 +0530

    Update asmvec.c

--- vector_c/asmvec.c
+++ vector_c/asmvec.c
@@ -10,10 +10,11 @@ int main() {
 
     for (int i = 0; i < N; i++) {
         vector_ins(&v, &i);   // renamed from vector_insert to vector_ins
+        // still facing a conflict, linked-list was named vector_insert......!!!
     }
 
     int last;
-    vector_get(&v, N - 1, &last);
+    vector_get(&v, N - 1, &last); // yeah working
     printf("asm last = %d\n", last);
 
     vector_free(&v);
